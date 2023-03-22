abdec5d312ee64ae1d31d6abb0e5b996dfb7690c
Cizzeros <gautamcizal@gmail.com>
2023-03-22 14:55:49 +0545

    modal complete

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { closeModal } from "../features/modal/modalSlice";
+import { clearCart } from "../features/cart/cartSlice";
 
 const Modal = () => {
+
+  const dispatch = useDispatch()
+
   return (
     <aside className='modal-container'>
       <div className='modal'>
@@ -9,12 +15,19 @@ const Modal = () => {
           <button
             type='button'
             className='btn confirm-btn'
+            onClick={() => {
+              dispatch(clearCart())
+              dispatch(closeModal())
+            }}
           >
             confirm
           </button>
           <button
             type='button'
             className='btn clear-btn'
+            onClick={() => {
+              dispatch(closeModal())
+            }}
           >
             cancel
           </button>

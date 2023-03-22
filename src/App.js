49000616fa7bd1dbb692a5e4057ada9f76f39326
Cizzeros <gautamcizal@gmail.com>
2023-03-22 14:30:10 +0545

    created modal component and modalSlice

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 import {useSelector, useDispatch} from 'react-redux'
 import { calculateTotals } from './features/cart/cartSlice';
+import Modal from './components/Modal';
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart)
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
